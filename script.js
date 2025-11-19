029d4518267d7a5947573cf0b76ee8a53fc013e8
Aaryan Pashine <pashineaaryan@gmail.com>
2025-11-19 15:31:30 -0500

    omg pliz save

--- script.js
+++ script.js
@@ -174,8 +174,10 @@ let h2 = t => ['h2', t]
 let h3 = t => ['h3', t]
 let h4 = t => ['h4', t]
 let video = t => ['video', {src: t, muted: true, autoplay: true, loop: true }]
+let videof = t => ['video', {src: "./images/" +t+".mp4", muted: true, autoplay: true, loop: true }]
 let img = t => ['img', {src: t}]
 let imgf = t => ['img', {src:"./images/" +t+".png"}]
+let imgfj = t => ['img', {src:"./images/" +t+".jpeg"}]
 let giff = t => ['img', {src:"./images/" +t+".gif"}]
 let light = t => ['span.light', t]
 
@@ -183,7 +185,7 @@ let intro = [
 	[display.cd(flash("HELLO WORLD"))],
 	[display.cd(flash("aaryan"))],
 	[display.dd(h2("I'm a graphic design student"))],
-	[display.cd(h1("And I also code" ))],
+	[display.cd(h1("And a self-taught programmer" ))],
 	[
 		// add gifs of the tool
 		display.cd(empty),
@@ -193,12 +195,13 @@ let intro = [
 		display.dc(h1('Publication Tool'))
 	],
 
-	[display.dd(h1("PARTS"))],
-	[display.dd(
-		['div', 
-		h1("PARTS &"),
-		h1("PROCESS")]
-	)],
+	[
+		display.dd(h1("PARTS")),
+		display.cd(imgf("parts"))
+	],
+	[display.dd(['div', h1("PARTS &"), h1("PROCESS"),]),
+	display.cd(imgf("slides"))
+	],
 
 	[display.clear],
 	[focus_design, display.dc(h2('The tool...'))],
@@ -244,7 +247,7 @@ let build = (items, fn /*(total)*/) => items.reduce((acc, item) => {
 			slides:[],
 		}).slides
 
-let introthesecond = [
+let process = [
 	[display.clear],
 	[display.cd(flash('Bits and Pieces'))],
 	[normal_layout],
@@ -279,7 +282,7 @@ let introthesecond = [
 
 	[focus_coding],
 	[display.cc(h1("OK"))],
-	[display.cc(h1("Syntax"))],
+	[display.cc(h1("WORDS"))],
 
 	...build([
 			h4("Break down this vocabulary"),
@@ -288,6 +291,8 @@ let introthesecond = [
 			h4("v"),
 			h2("how to constitute in a programming context")],
 					 total => [display.cd(['div', ...total])]),
+	
+	[display.cd(h1("USE WORDS to reconstitute P5 CANVAS as a BOOK"))],
 ]
 
 let text = [
@@ -344,6 +349,9 @@ let units = [
 	[display.dc(h3("Units give this p5 canvas the validity of a letter size sheet"))],
 
 
+]
+
+let spread = [
 	[display.dc(h3("So we have a sheet!"))],
 	[display.dc(h3("But we're making books...")),
 		display.cd(empty),
@@ -374,24 +382,137 @@ let units = [
 		display.dd(giff('recto_verso')),
 	],
 
-	[focus_coding],
-
-	[display.cc(h3("I need to be able to access, verso and recto"))],
 ]
 
+let grid = [
+	[focus_coding],
+	[display.cc(h4('Constitute canvas as two pages'))],
+	[display.cc(h4('Verso and Recto'))],
+	[display.cc(h4('Validate canvas as a Spread'))],
+
+	[display.cd(h1('GRID'))],
+	[display.cd(h1('*Show Grid images*'))],
+	[display.cd(
+		imgf('simple_grid')
+	)],
+	[display.cd(
+		['div', 
+		 imgf('simple_grid'),
+		 h4('So the grid is a structure that helps us locate points on a page.')
+		])],
+
+	[display.cd(imgf('grid'))],
+	[display.cd(imgf('grid_diagram'))],
+	[display.cd(imgf('grid_use'))],
+	[display.cc(h4('Validate canvas as a Spread ✅'))],
+]
+
+let sequencing = [
+	[display.clear],
+	[display.cc(h4('this one is easy'))],
+	[display.cc(h4("So up until now we've been doing a single spread"))],
+	[
+		display.cc(h4("But a book has multiple")),
+		display.cd(h4("*Flupping througbn book")),
+	],
+
+	[display.cc(h4("Store a spread as data")),],
+	[display.cd(imgf("spread_drawable_item")),],
+	[display.cd(['div',
+							 imgf("spread_drawable_item"), (h4("Drawables use grid to find positions"))
+							]),],
+
+	[display.cd(imgf("spread_data")),],
+	[display.cd(imgf("draw_spread")),],
+	[display.cd(imgf("draw_book")),],
+	[display.cd(imgf("spreads_example")),],
+	[display.cd(videof("spread_sequence")),],
+
+	[display.cc(h4("So we're done right?")),],
+	[display.cc(h4("We can print this out and we're good?")),],
+	[display.cc(h4("Right?")),],
+]
+
+let imposition = [
+	[focus_design],
+	[display.dc(h4("Except there's one critical thing."))],
+	[display.dc(h4("What we percieve as spreads are separate physical sheets")),
+	 display.dd(videof("spread_sheets")),
+	 display.cd(videof("spread_sequence")),
+	],
+	[normal_layout],
+	[display.cc(h3('reconcile!'))],
+	[display.cc(h3('Design it in this format'))],
+	...build(['1', '2', '3', '4', '5'],
+					 (total) => [
+						 display.cd(imgf('impose/digital/' + total[total.length-1])),
+						 display.dd(img('./images/impose/physical/'
+														+ total[total.length-1] + '.jpeg')),
+					 ]),
+
+
+	[display.cc(h3('Print it as reconfigured')),
+	 display.dc(h3('For binding'))],
+	...build(['1', '2', '3', '4'],
+					 (total) => [
+						 display.cd(imgf('impose/digital/s' + total[total.length-1])),
+						 display.dd(imgf('impose/physical/s' + total[total.length-1]))
+					 ]),
+	[display.clear],
+	[display.cc(h4('Ok so how do we figure out this configuration?'))],
+
+	[
+		display.dc(h4('This is the algorithm for it')),
+		display.dd(videof('shuffle_sped_up'))
+	],
+	[
+		display.cc(h4('This is also the algorithm for it')),
+		display.cd(imgfj('sketchbook'))
+	],
+
+	[
+		display.cc(h4("It's just a nuanced switcheroo")),
+		display.cd(imgfj('switcheroo'))
+	],
+
+	[focus_coding],
+
+	[display.cc(h4("Implementation")), display.cd(imgf('imposition'))],
+	[display.cc(h4("Render Pairs")), display.cd(imgf('render_imposed'))],
+
+]
 let parts = [
 	[focus_coding, display.clear],
 	[display.cd(flash('THE PARTS'))],
 	...text,
 	...units,
+	...spread,
+	...grid,
+	...sequencing,
+	...imposition
+]
+
+let conclusions = [
+	[normal_layout, display.clear],
+	[display.cd(flash('THATS ABOUT')), display.dd(flash('ALL THE PARTS'))],
+	[display.cc(h3('to think of the p5 canvas'))],
+	[display.dc(h3('as a book'))],
+	[display.cc(h3('and to print it out'))],
+	[display.dc(h3('and make a book'))],
+
+	[display.cd(flash('CLosing thoughts'))],
+	[display.cc(h1('Play around with'))],
+	[display.dc(h1('Term definitions'))],
+	// [display.cc(imgf(''))],		
 ]
 
 let slides = [
 	[],
 	...intro,
 	...demo,
-	...introthesecond,
+	...process,
 	...parts,
+	...conclusions
 ]
 
 
@@ -431,6 +552,6 @@ let gotoslide = e => {
 
 document.body.appendChild(dom(root))
 
-// gotoslide(94)
+// gotoslide(140)
 
 
--- temp.js
+++ temp.js
@@ -1,6 +1,6 @@
 
 // text(text, x, y)
-   text("Book's have words", 10, 20)
+text("Book's have words", 10, 20)
 //                            ^   ^
 //                            |   |
 //  pixels ------------------------
@@ -8,13 +8,13 @@
 
 // PPI -> Pixels Per Inch
 // UNITS
-let inch  = (v) => v * ppi 
-let em    = (v) => inch(v) / 6
-let pica  = (v) => em(v)
+let inch = (v) => v * ppi
+let em = (v) => inch(v) / 6
+let pica = (v) => em(v)
 let point = (v) => pica(v) / 12
 
 
-text("Book's have words", inch(1/2), inch(1/4))
+text("Book's have words", inch(1 / 2), inch(1 / 4))
 //                              ^          ^
 //                              |          |
 //  half inch -------------------          |
@@ -27,3 +27,90 @@ text("Book's have words", inch(1/2), inch(1/4))
 createCanvas(inch(11), inch(8.5))
 
 
+let grid = ({
+	width,
+	height,
+	columnCount,
+	hanglineCount,
+	margins,
+}) => ({
+	// recto -> width / 2 + margins... etc
+	recto         /* x position */,
+	verso         /* x position */,
+	rectoColumns  /* x positions */,
+	versoColumns  /* x positions */,
+	hanglines     /* y positions */,
+})
+
+
+let Grid = grid({
+	width: inch(10),
+	height: inch(8),
+	...others
+})
+
+text("some other text",
+	Grid.recto + inch(1),
+	inch(.5))
+
+//or
+text("some other text",
+	Grid.rectoColumns(2),
+	inch(.5))
+
+
+let spread = [
+	['text',
+
+		['text',
+			"there's a circle on the spread!"],
+
+		['x', ['rectoColumn', 2]],
+		['y', ['inch', 1]]
+	],
+
+	['circle',
+		['radius', ['inch', .5]]
+		['x', ['rectoColumn', 2]],
+		['y', ['inch', 1]]
+	],
+]
+
+
+
+
+let drawSpread = (contents, grid) => {
+	// clears screen
+	// loops over content and draws each of them
+}
+
+
+
+
+
+
+let drawBook = (spreads, pg) =>
+	drawSpread(spreads[pg])
+
+
+
+let versoImage = spread => {
+  // draw spread to a buffer
+  // clip out the verso region using grid
+  // return image
+}
+
+let rectoImage = spread => {/*same stuff*/}
+
+let drawPair = (
+	spreads,
+	[rectoPage, versoPage]
+) => {
+  // get the recto and verso images using spreads
+  let recto = rectoImage(spreads[rectoPage])
+  let verso = versoImage(spreads[versoPage])
+
+  // draw them on current canvas
+  img(verso, 0, 0)
+  img(recto, grid.recto, 0)
+}
